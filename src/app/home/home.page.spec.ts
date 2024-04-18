1731f71ea131eb83c16c8410cba833e58fc99150
di-birt <mardanza@birt.eus>
2024-04-18 10:00:56 +0200

    comentarios

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -28,14 +28,19 @@ describe('HomePage', () => {
     fixture.detectChanges();
   });
 
+  /***Recordar que en este caso component hace referencia a HomePage ***/
+  //Test añadido por defecto que comprueba que el componente existe.
   it('should create HomePage component', () => {
     expect(component).toBeTruthy();
   });
 
+  //En home.page.ts definiremos una variable titulo que se inicialice con este texto
   it('Debería tener el titulo "Mi primer testing"', () => {
     expect(component.titulo).toEqual('Mi primer testing');
   });
 
+  /***Recordar que tenemos que añadir la variable, añadir el componente en providers y hacer el inject del TestBed ***/
+  //Hacemos uso de MiComponente para comprobar que tiene que existir una variable "miVariable" de tipo array y que esté iniciado a vacío
   it('Debería existir en MiComponente una variable llamado "miVariable" y se inicialice como un array vacío',() => {
     //Comprobamos que exista la variable
     expect(miComponente.miVariable).toBeTruthy();
@@ -43,10 +48,14 @@ describe('HomePage', () => {
     expect(miComponente.miVariable).toEqual([]);
   });
 
+  //Mediante querySelector, podemos testear nuestro HTML.
+  //En este caso tenemos que tener un div, dentro un p y dentro un texto "Hola Mundo"
   it('En el Html debería imprimir "Hola Mundo" con un div y una p', () => {
     expect(compiled.querySelector('div > p').textContent).toContain('Hola Mundo');
   });
 
+  //Si queremos testear la lógica del método sumar, podemos crear un objeto mock con ambos valores.
+  //Esto es lo mismo que hacer directamente: miComponente.suma(2, 3);
   it('Debería de sumar dos número', () => {
     const mockedData = {num1: 2, num2: 3};
     const resultadoEsperado = 5;
